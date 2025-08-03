b2fd208d632828dc4089f1030f73b0dc43d141d7
Abdul Rafay Ahmed <111084742+Rafay575@users.noreply.github.com>
2025-08-04 00:17:48 +0500

    Initial commit

---
+++ app/contact/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContactForm from "@/components/ContactForm"
+import Location from "@/components/Location"
+
+export default function page() {
+  return (
+    <div className='w-[100vw] '>
+    <ContactForm/>
+    <Location/>
+    </div>
+  )
+}
